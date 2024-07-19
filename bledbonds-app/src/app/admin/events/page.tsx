f5bb9f76ff24ef72b90239e70e2bb48cdfa257ab
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2024-07-19 23:42:43 +0200

    events page design idea

---
+++ bledbonds-app/src/app/admin/events/page.tsx
@@ -0,0 +1,22 @@
+export default function EventsPage () {
+    return (
+        // Tabla de eventos
+        /*
+        Nombre | Fecha | Lugar | Descripción | Ver participantes | Ver chat | Editar | Eliminar
+        */
+        /*
+        Flujo al abrir el chat:
+            1. Abre un modal con el chat
+            2. Llamar API para obtener los mensajes del chat
+            3. Llamar API para enviar mensajes (enviar mensajes en tiempo real)
+        */
+        // Botón para crear un nuevo evento --> abre un modal
+        /*
+        Flujo al crear un evento llamar API:
+        1. Llamar API al subir la imagen
+        2. Guardar la URL de la imagen que da la API
+        3. Llamar API al crear el evento
+        */
+        <></>
+    )
+}
