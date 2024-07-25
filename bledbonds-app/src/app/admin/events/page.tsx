1b29ddff137ea60aed155d7671e90d9eb30627a9
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-07-25 23:53:22 +0200

    feat(): add chat

--- bledbonds-app/src/app/admin/events/page.tsx
+++ bledbonds-app/src/app/admin/events/page.tsx
@@ -124,7 +124,7 @@ export default function EventsPage() {
   }, [])
 
   useEffect(() => {
-    const socket = io('http://localhost:3001');
+    const socket = io('https://api.bledbonds.es');
     setSocket(socket);
   }, []);
   useEffect(() => {
