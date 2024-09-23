7f05f273dc254808bc763614cb84f3947ed42cf0
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-23 18:31:31 +0200

    Create styled Button

--- bledbonds-app/src/app/UX/Link/index.tsx
+++ bledbonds-app/src/app/UX/Link/index.tsx
@@ -1,4 +1,9 @@
-import { Button } from "@mui/material";
+'use client'
+
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import Button, { ButtonProps } from "@mui/material/Button";
+import { purple } from "@mui/material/colors";
+import { useEffect, useState } from "react";
 
 interface LinkProps {
   href: string;
@@ -6,15 +11,27 @@ interface LinkProps {
   className?: string;
 }
 
-export default function Link (props: Readonly<LinkProps>) {
+const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? 'red' : '#002b33',
+  backgroundColor: '#04bbc8',
+  padding: '0.5rem',
+  '&:hover': {
+    backgroundColor: '#0093a1',
+  },
+  '&:active': {
+    backgroundColor: '#057480',
+  }
+}));
 
+export default function Link (props: Readonly<LinkProps>) {
   return (
-    <Button
+    <ColorButton
       variant="contained"
       href={props.href}
       className={`${props.className}`}
+      fullWidth
     >
       {props.children}
-    </Button>
+    </ColorButton>
   )
 }
