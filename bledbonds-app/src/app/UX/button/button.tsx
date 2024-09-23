50394cdbbbd07fef025aa61159f122331a7ab1fa
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-23 17:41:45 +0200

    Modificar componente Button para que tenga el estilo de mui

--- bledbonds-app/src/app/UX/button/button.tsx
+++ bledbonds-app/src/app/UX/button/button.tsx
@@ -1,3 +1,5 @@
+import MuiButton from "../MuiButton/MuiButton";
+
 interface ButtonProps {
   label?: string;
   onClick: () => any;
@@ -5,6 +7,8 @@ interface ButtonProps {
   className: string;
   noBorder?: boolean;
   disabled?: boolean;
+  mui?: boolean;
+  href?: string;
 }
 
 export default function Button(props: Readonly<ButtonProps>) {
@@ -20,9 +24,24 @@ export default function Button(props: Readonly<ButtonProps>) {
     }
   }
 
-  return (
-    <button className={`bg-palette-500 hover:bg-palette-600 active:bg-palette-700 disabled:bg-palette-100 dark:disabled:bg-palette-950 text-palette-950 disabled:text-palette-400 dark:disabled:text-palette-700 border-solid border-2 border-palette-500 hover:border-palette-600 active:border-palette-700 disabled:border-palette-100 dark:disabled:border-palette-900 p-2 rounded-lg ${props.noBorder ? 'mb-0' : 'mb-3'} ${props.className}`} onClick={() => { props.onClick() }} disabled={props.disabled ? props.disabled : false}>
-      {getChildren()}
-    </button>
-  );
+  if (props.mui) {
+    return (
+      <MuiButton
+        onClick={props.onClick}
+        className={props.className}
+        noBorder={props.noBorder}
+        disabled={props.disabled}
+        href={props.href}
+      >
+        {getChildren()}
+      </MuiButton>
+    )
+  } else {
+    return (
+      <button className={`bg-palette-500 hover:bg-palette-600 active:bg-palette-700 disabled:bg-palette-100 dark:disabled:bg-palette-950 text-palette-950 disabled:text-palette-400 dark:disabled:text-palette-700 border-solid border-2 border-palette-500 hover:border-palette-600 active:border-palette-700 disabled:border-palette-100 dark:disabled:border-palette-900 p-2 rounded-lg ${props.noBorder ? 'mb-0' : 'mb-3'} ${props.className}`} onClick={() => { props.onClick() }} disabled={props.disabled ? props.disabled : false}>
+        {getChildren()}
+      </button>
+    );
+  }
+
 }
