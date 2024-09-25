6a968275c1c90a9fc05b63378f548a71067ec56d
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-25 16:34:53 +0200

    API form delete

---
+++ bledbonds-app/src/app/api/delete/deleteUserAPI.ts
@@ -0,0 +1,32 @@
+const deleteUserAPI = async (code: {email: string, phone: string}) => {
+  try {
+    const host = window.location.host;
+    let API_URL = '';
+    
+    if (host.includes('localhost:3000')) {
+      API_URL = 'http://localhost:3001/api/v1';
+    } else {
+      API_URL = 'https://api.bledbonds.es/api/v1';
+    }
+    console.log(code);
+    
+    const resp = await fetch(`${API_URL}/users/delete/form`, {
+      method: 'POST',
+      headers: {
+        'x-api-key': '6d83d4496c0010950eb2f3a0db79004c',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(code)
+    });
+    if (!resp.ok) {
+      throw new Error('Network response was not ok');
+    } else {
+      const data = await resp.json();
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default deleteUserAPI;
