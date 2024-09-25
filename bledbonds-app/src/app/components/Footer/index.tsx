4657329ae56722ce4b98e33423c09b1f0294cf46
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-25 16:36:31 +0200

    actualizar footer

--- bledbonds-app/src/app/components/Footer/index.tsx
+++ bledbonds-app/src/app/components/Footer/index.tsx
@@ -4,6 +4,52 @@ import Image from "next/image"
 import Link from "next/link"
 
 export default function Footer() {
+
+  const interests = [
+    {
+      name: 'Newsletter',
+      url: '/newsletter',
+      alternative: 'Suscribete a nuestro newsletter'
+    },
+    {
+      name: 'Register',
+      url: '/register',
+      alternative: 'Registrate'
+    },
+    {
+      name: 'Blog',
+      url: '/blog',
+      alternative: 'Blog'
+    },
+    {
+      name: 'Eliminar mi cuenta',
+      url: '/blog/delete',
+      alternative: 'Como eliminar mi cuenta'
+    },
+    {
+      name: 'Actualizar mi cuenta',
+      url: '/blog/update',
+      alternative: 'Como actualizar mi cuenta'
+    }
+  ]
+
+  const follow = [
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/bledbonds_app/',
+      icon: 'instagram',
+      alternative: 'Instagram'
+    }
+  ]
+
+  const policy = [
+    {
+      name: 'Privacidad',
+      url: '/privacidad',
+      alternative: 'Política de privacidad'
+    },
+  ]
+
   const footer = [
     {
       name: 'newsletter',
@@ -18,6 +64,14 @@ export default function Footer() {
     {
       name: 'blog',
       url: '/blog'
+    },
+    {
+      name: 'política de privacidad',
+      url: '/privacidad'
+    },
+    {
+      name: 'como eliminar mi cuenta',
+      url: '/delete'
     }
     // {
     //     name: 'login',
@@ -41,45 +95,72 @@ export default function Footer() {
   return (
     <>
       <hr className="border-palette-10 dark:border-palette-50" />
-      <footer className="bg-palette-3 dark:bg-palette-950 shadow">
-        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-              <Image src="/logo.png" width={32} height={32} alt="Flowbite Logo" className="h-8" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-palette-11 dark:text-palette-50">BLEDBONDS</span>
-            </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-palette-11 dark:text-palette-50 sm:mb-0">
-              {
-                footer.map((item, index) => (
-                  <li key={index}>
-                    <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
-                      {item.name}
-                    </Link>
-                  </li>
-                ))
-              }
-            </ul>
+
+      <footer className="bg-palette-3 dark:bg-palette-950">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+          <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+              <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <Image src="/logo.png" width={32} height={32} alt="Flowbite Logo" className="h-8" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-palette-11 dark:text-palette-50">BLEDBONDS</span>
+              </a>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-palette-950 uppercase dark:text-palette-50">Paginas de interés</h2>
+                <ul className="text-palette-950 dark:text-palette-50 font-medium">
+                  {interests.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-palette-950 uppercase dark:text-palette-50">Siguenos</h2>
+                <ul className="text-palette-950 dark:text-palette-50 font-medium">
+                  {follow.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-sm font-semibold text-palette-950 uppercase dark:text-palette-50">Politicas</h2>
+                <ul className="text-palette-950 dark:text-palette-50 font-medium">
+                  {policy.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
-          <hr className="my-6 border-palette-11 dark:border-palette-50 sm:mx-auto lg:my-8" />
-          <ul className="flex content-center justify-center items-center mb-6 text-sm font-medium text-palette-11 dark:text-palette-50 sm:mb-0">
-            {
-              social.map((item, index) => (
-                <li key={index} className="flex">
-                  <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
+          <hr className="my-6 border-palette-950 sm:mx-auto dark:border-palette-50 lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="block text-sm text-palette-11 dark:text-palette-1 sm:text-center">© {new Date().getFullYear()} <a href="/" className="hover:underline">BLEDBONDS™</a>. All Rights Reserved.</span>
+            <div className="flex mt-4 sm:justify-center sm:mt-0">
+              {
+                social.map((item, index) => (
+                  <a href={item.url} className="text-palette-950 hover:text-palette-900 dark:text-palette-50 dark:hover:text-palette-100" key={index + 1}> 
                     {
                       item.icon === 'instagram' ? (
                         <Instagram />
                       ) : item.name
                     }
-                  </Link>
-                </li>
-              ))
-            }
-          </ul>
-          <hr className="my-6 border-palette-11 dark:border-palette-50 sm:mx-auto lg:my-8" />
-          <span className="block text-sm text-palette-11 dark:text-palette-1 sm:text-center">© {new Date().getFullYear()} <a href="/" className="hover:underline">BLEDBONDS™</a>. All Rights Reserved.</span>
-          <hr className="my-6 border-palette-11 dark:border-palette-50 sm:mx-auto lg:my-8" />
-          <span className="block text-sm text-palette-11 dark:text-palette-1 sm:text-center">Developed by: <a href="https://github.com/bledbonds" className="hover:underline">ruugii</a></span>
+                  </a>
+                ))
+              }
+            </div>
+          </div>
         </div>
       </footer>
     </>
