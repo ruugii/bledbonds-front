52bc7b74a57b37713215c11b4c6197de29490bbe
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-25 16:34:25 +0200

    agregar paginas /blog/delete y /blog/update en preview

--- bledbonds-app/src/app/components/BlogPreview/index.tsx
+++ bledbonds-app/src/app/components/BlogPreview/index.tsx
@@ -63,6 +63,69 @@ export default function BlogPreview() {
           </div>
         </article>
       ))}
+      <article className="p-6 bg-palette-11 dark:bg-palette-900 rounded-lg border border-palette-9 dark:border-palette-800 shadow-md">
+        <div className="flex justify-between items-center mb-5 text-palette-2">
+          <span className="bg-palette-10 dark:bg-palette-9 text-palette-2 dark:text-palette-50 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+            <Category category='gestion de usuarios' />
+          </span>
+        </div>
+        <Subtitle margin bold traking className="text-palette-50">
+          <a href={`/blog/delete`}>ELIMINAR MI CUENTA</a>
+        </Subtitle>
+        <p className="mb-5 font-light text-palette-1">
+          Quiero eliminar mi cuenta de BledBonds, ¿cómo lo puedo hacer?
+        </p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-4">
+              <div className="w-7 h-7 rounded-full bg-palette-10 dark:bg-palette-2 flex items-center justify-center">
+                <span className="text-palette-2 dark:text-palette-900 font-medium">
+                  B
+                </span>
+              </div>
+            <span className="font-medium text-palette-1">
+              BledBonds
+            </span>
+          </div>
+          <Link href={`/blog/delete`} className="inline-flex items-center font-medium text-palette-1 hover:underline">
+            Read more
+            <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+            </svg>
+          </Link>
+        </div>
+      </article>
+
+      <article className="p-6 bg-palette-11 dark:bg-palette-900 rounded-lg border border-palette-9 dark:border-palette-800 shadow-md">
+        <div className="flex justify-between items-center mb-5 text-palette-2">
+          <span className="bg-palette-10 dark:bg-palette-9 text-palette-2 dark:text-palette-50 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+            <Category category='gestion de usuarios' />
+          </span>
+        </div>
+        <Subtitle margin bold traking className="text-palette-50">
+          <a href={`/blog/update`}>ACTUALIZAR MI CUENTA</a>
+        </Subtitle>
+        <p className="mb-5 font-light text-palette-1">
+          Quiero actualizar mi cuenta de BledBonds, ¿cómo lo puedo hacer?
+        </p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-4">
+              <div className="w-7 h-7 rounded-full bg-palette-10 dark:bg-palette-2 flex items-center justify-center">
+                <span className="text-palette-2 dark:text-palette-900 font-medium">
+                  B
+                </span>
+              </div>
+            <span className="font-medium text-palette-1">
+              BledBonds
+            </span>
+          </div>
+          <Link href={`/blog/update`} className="inline-flex items-center font-medium text-palette-1 hover:underline">
+            Read more
+            <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+            </svg>
+          </Link>
+        </div>
+      </article>
     </div>
   )
 }
