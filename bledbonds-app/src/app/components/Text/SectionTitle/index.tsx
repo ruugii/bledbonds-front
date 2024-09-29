cb5b51b0fd81b54b6d0c8dbd590ae6b7e9fc35c4
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-09-29 14:09:14 +0200

    add classname

--- bledbonds-app/src/app/components/Text/SectionTitle/index.tsx
+++ bledbonds-app/src/app/components/Text/SectionTitle/index.tsx
@@ -5,6 +5,7 @@ interface SectionTitleProps {
   margin?: boolean;
   lg?: boolean;
   left?: boolean;
+  className?: string;
 }
 
 export default function SectionTitle(props:SectionTitleProps) {
@@ -16,7 +17,7 @@ export default function SectionTitle(props:SectionTitleProps) {
     lg
   } = props;
   return (
-    <h3 className={`${lg ? 'text-lg' : 'text-2xl'} ${bold && 'font-bold'} text-palette-11 dark:text-palette-50 ${width && 'w-[80%]'} ${margin && 'mt-3'} ${props.left && 'w-full text-left'}`}>
+    <h3 className={`${lg ? 'text-lg' : 'text-2xl'} ${bold && 'font-bold'} text-palette-11 dark:text-palette-50 ${width && 'w-[80%]'} ${margin && 'mt-3'} ${props.left && 'w-full text-left'} ${props.className && props.className}`}>
       {children}
     </h3>
   )
