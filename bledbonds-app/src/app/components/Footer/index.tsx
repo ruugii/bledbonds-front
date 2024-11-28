d9d3003b509be30c9dfca849f15fcb4ed7beb349
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-11-28 18:35:43 +0100

    actualizar footer

--- bledbonds-app/src/app/components/Footer/index.tsx
+++ bledbonds-app/src/app/components/Footer/index.tsx
@@ -1,4 +1,6 @@
 import Instagram from "@/app/Icons/instagram"
+import Linkedin from "@/app/Icons/linkedin"
+import Twitter from "@/app/Icons/Twitter"
 import Menu from "@/app/interfaces/menu"
 import Image from "next/image"
 import Link from "next/link"
@@ -30,11 +32,24 @@ export default function Footer() {
       name: 'Actualizar mi cuenta',
       url: '/blog/update',
       alternative: 'Como actualizar mi cuenta'
-    },
+    }
+  ]
+
+  const events = [
     {
       name: 'Eventos',
       url: '/events',
       alternative: 'Eventos'
+    },
+    {
+      name: 'Proximos eventos',
+      url: '/events/next',
+      alternative: 'Proximos eventos'
+    },
+    {
+      name: 'Eventos pasados',
+      url: '/events/prev',
+      alternative: 'Eventos pasados'
     }
   ]
 
@@ -44,6 +59,17 @@ export default function Footer() {
       url: 'https://www.instagram.com/bledbonds_app/',
       icon: 'instagram',
       alternative: 'Instagram'
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/company/bledbonds/',
+      icon: 'linkedin',
+      alternative: 'Linkedin'
+    }, {
+      name: 'X',
+      url: 'https://x.com/bledbonds_app',
+      icon: 'twitter',
+      alternative: 'Twitter'
     }
   ]
 
@@ -55,45 +81,24 @@ export default function Footer() {
     },
   ]
 
-  const footer = [
-    {
-      name: 'newsletter',
-      url: '/newsletter',
-      alternative: 'Suscribete a nuestro newsletter'
-    },
-    {
-      name: 'register',
-      url: '/register',
-      alternative: 'Registrate'
-    },
-    {
-      name: 'blog',
-      url: '/blog'
-    },
-    {
-      name: 'política de privacidad',
-      url: '/privacidad'
-    },
-    {
-      name: 'como eliminar mi cuenta',
-      url: '/delete'
-    }
-    // {
-    //     name: 'login',
-    //     url: '/login'
-    // }
-    // {
-    //     name: 'register',
-    //     url: '?register=true'
-    // }
-  ] as Menu[]
-
   const social = [
     {
       name: 'Instagram',
       url: 'https://www.instagram.com/bledbonds_app/',
       icon: 'instagram',
       alternative: 'Instagram'
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/company/bledbonds/',
+      icon: 'linkedin',
+      alternative: 'Linkedin'
+    },
+    {
+      name: 'Twitter',
+      url: 'https://x.com/bledbonds_app',
+      icon: 'twitter',
+      alternative: 'Twitter'
     }
   ]
 
@@ -105,11 +110,23 @@ export default function Footer() {
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <Image src="/logo.png" width={32} height={32} alt="Flowbite Logo" className="h-8" />
+                <Image src="/logo.png" width={32} height={32} alt="BledBonds Logo" title="BledBonds Logo" className="h-8" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap text-palette-11 dark:text-palette-50">BLEDBONDS</span>
               </a>
             </div>
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div className="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+              <div>
+                <p className="mb-6 text-sm font-semibold text-palette-950 uppercase dark:text-palette-50">Eventos</p>
+                <ul className="text-palette-950 dark:text-palette-50 font-medium">
+                  {events.map((item, index) => (
+                    <li key={index}>
+                      <Link href={item.url} className="hover:underline me-4 md:me-6" aria-label={item.alternative}>
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
               <div>
                 <p className="mb-6 text-sm font-semibold text-palette-950 uppercase dark:text-palette-50">Paginas de interés</p>
                 <ul className="text-palette-950 dark:text-palette-50 font-medium">
@@ -154,10 +171,14 @@ export default function Footer() {
             <div className="flex mt-4 sm:justify-center sm:mt-0">
               {
                 social.map((item, index) => (
-                  <a href={item.url} className="text-palette-950 hover:text-palette-900 dark:text-palette-50 dark:hover:text-palette-100" key={index + 1}>
+                  <a href={item.url} className="text-palette-950 hover:text-palette-900 dark:text-palette-50 dark:hover:text-palette-100 mx-1" key={index + 1}>
                     {
                       item.icon === 'instagram' ? (
                         <Instagram />
+                      ) : item.icon === 'linkedin' ? (
+                        <Linkedin />
+                      ) : item.icon === 'twitter' ? (
+                        <Twitter />
                       ) : item.name
                     }
                   </a>
