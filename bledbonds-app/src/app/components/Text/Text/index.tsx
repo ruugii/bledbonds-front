82af9dc74f4f4502794cc308f9d0042c833d5905
ruugii_31 <roger.barrero.sorribas@gmail.com>
2024-11-28 18:43:27 +0100

    update component Text

--- bledbonds-app/src/app/components/Text/Text/index.tsx
+++ bledbonds-app/src/app/components/Text/Text/index.tsx
@@ -11,19 +11,20 @@ interface TextProps {
   bold?: boolean;
   subtitle?: boolean;
   center?: boolean;
+  right?: boolean;
 }
 
 export default function Text(props: TextProps) {
   if (typeof props.children === 'string') {
     return (
       <p
-      className={`text-palette-11 dark:text-palette-50 text-left ${props.margin ? 'mb-4' : ''} ${props.className} ${props.left && 'text-left'} ${props.justify && 'text-justify'} ${props.full && 'w-full'} ${props.big ? 'text-2xl' : props.subtitle ? 'text-3xl' : 'text-lg'} ${props.bold ? 'font-bold' : ''} ${props.center ? 'text-center' : ''}`}
+      className={`text-palette-11 dark:text-palette-50 text-left ${props.margin ? 'mb-4' : ''} ${props.className} ${props.left && 'text-left'} ${props.justify && 'text-justify'} ${props.full && 'w-full'} ${props.big ? 'text-2xl' : props.subtitle ? 'text-3xl' : 'text-lg'} ${props.bold ? 'font-bold' : ''} ${props.center ? 'text-center' : ''} ${props.right ? 'text-right' : ''}`}
         dangerouslySetInnerHTML={{ __html: props.children }}
       />
     );
   } else {
     return (
-      <p className={`text-palette-11 dark:text-palette-50 text-left ${props.margin ? 'mb-4' : ''} ${props.className} ${props.left && 'text-left'} ${props.justify && 'text-justify'} ${props.full && 'w-full'} ${props.big ? 'text-2xl' : 'text-lg'} ${props.center ? 'text-center' : ''}`}>{props.children}</p>
+      <p className={`text-palette-11 dark:text-palette-50 text-left ${props.margin ? 'mb-4' : ''} ${props.className} ${props.left && 'text-left'} ${props.justify && 'text-justify'} ${props.full && 'w-full'} ${props.big ? 'text-2xl' : 'text-lg'} ${props.center ? 'text-center' : ''} ${props.right ? 'text-right' : ''}`}>{props.children}</p>
     )
   }
 }
